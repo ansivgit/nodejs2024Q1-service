dd168302ed9f775399271cb34862be00793bab36
Anna Zhuravleva <ansiv@yandex.ru>
2024-03-08 16:02:12 +0300

    feat: add swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
+import 'dotenv/config';
+
 import { AppModule } from './app.module';
+import { getSwaggerDoc } from './helpers';
+
+const PORT = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+
+  const swaggerDoc = await getSwaggerDoc();
+  SwaggerModule.setup('doc', app, swaggerDoc);
+
+  await app.listen(PORT);
 }
+
 bootstrap();
